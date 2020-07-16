1f3f1b3c3d945776870b95473f8831c4a7365453
Student_MA102 <aidarous12@gmail.com>
2020-07-16 11:42:10 -0400

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Firstgen from './components/Firstgen';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Firstgen />
-      </header>
-    </div>
-  );
+export const  UsernameContext = React.createContext('Aidarous');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          
+          <Firstgen />
+          
+          
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
